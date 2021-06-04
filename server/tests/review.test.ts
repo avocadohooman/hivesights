b507b80ae116d79444a0e7abde2d87e236428fc4
gerhard.molin <gmolin@student.hive.fi>
2021-06-04 14:24:35 +0300

    #skip optimising review testing suite

--- server/tests/review.test.ts
+++ server/tests/review.test.ts
@@ -12,21 +12,28 @@ const companyTable = 'company_test';
 const reviewColumns = reviewDBQueries.reviewColumns;
 const apiBaseUrl = '/api/reviews';
 
-//clearing review database and fetching correct company ids once before all tests
+// Getting latest company ids from company_table
+beforeAll(async () => {
+    try {
+        console.log(`fetching latest company ids from ${companyTable}`);
+        const getWunderdogId = await pool.query(`SELECT id FROM ${companyTable} WHERE companyname = ($1)`, ['Wunderdog']);
+        const getFuturiceId = await pool.query(`SELECT id FROM ${companyTable} WHERE companyname = ($1)`, ['Futurice']);
+        console.log(`Got Wunderdog ID: ${getWunderdogId.rows[0].id} and Futurice ID: ${getFuturiceId.rows[0].id}`);
+        helper.intialReviews[0].companyId = getWunderdogId.rows[0].id;
+        helper.intialReviews[1].companyId = getWunderdogId.rows[0].id;
+        helper.intialReviews[2].companyId = getFuturiceId.rows[0].id;
+        helper.intialReviews[3].companyId = getFuturiceId.rows[0].id;
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
+})
+
+// clearing review database and fetching correct company ids once before all tests
 beforeEach(async () => {
     try {
         console.log(`clearing ${reviewTable}`);
         await pool.query(`DELETE FROM ${reviewTable}`);
         console.log(`${reviewTable} cleared`);
-
-        console.log(`fetching latest company ids from ${companyTable}`);
-        const wunderDogId = await pool.query(`SELECT id FROM ${companyTable} WHERE companyname = ($1)`, ['Wunderdog']);
-        const futuriceId = await pool.query(`SELECT id FROM ${companyTable} WHERE companyname = ($1)`, ['Futurice']);
-        console.log(`Got Wunderdog ID: ${wunderDogId.rows[0].id} and Futurice ID: ${futuriceId.rows[0].id}`);
-        helper.intialReviews[0].companyId = wunderDogId.rows[0].id;
-        helper.intialReviews[1].companyId = wunderDogId.rows[0].id;
-        helper.intialReviews[2].companyId = futuriceId.rows[0].id;
-        helper.intialReviews[3].companyId = futuriceId.rows[0].id;
         
         console.log(`Populating ${reviewTable}`);
         await helper.populatingTable();
