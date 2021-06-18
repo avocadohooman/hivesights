090c74f5467a5af9fb784f4cc3699f32b5701208
avocadohooman <gmolin@student.hive.fi>
2021-06-18 14:06:16 +0200

    #skip sub ratings have now precision of .5

--- client/src/components/Companies/CompanySubRatings.tsx
+++ client/src/components/Companies/CompanySubRatings.tsx
@@ -13,7 +13,6 @@ import '../../styles/company.css';
 import '../../styles/companyDetailView.css'
 
 // UI Libraries
-import { makeStyles } from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
@@ -45,65 +44,37 @@ const CompanySubRatings = ({
                     
                     <Typography className="oneCompanySubRatingLabels"> 
                             Recruitment Process 
-                            <Tooltip className="oneCompantSubRatingTooltop" title={company[0].averageInterviewScore} placement="top">
-                                <div>
-                                    <Rating name="read-only" value={company[0].averageInterviewScore} readOnly />
-                                </div>
-                            </Tooltip>
+                            <Rating name="read-only" value={company[0].averageInterviewScore} precision={0.5} readOnly />
                     </Typography>
 
                     <Typography className="oneCompanySubRatingLabels"> 
                             Onboarding 
-                            <Tooltip className="oneCompantSubRatingTooltop" title={company[0].averageOnboardingScore} placement="top">
-                                <div>
-                                    <Rating name="read-only" value={company[0].averageInterviewScore} readOnly />
-                                </div>
-                            </Tooltip>
+                            <Rating name="read-only" value={company[0].averageOnboardingScore} precision={0.5} readOnly />
                     </Typography>
 
                     <Typography className="oneCompanySubRatingLabels"> 
                             Mentoring 
-                            <Tooltip className="oneCompantSubRatingTooltop" title={company[0].averageSupervisionScore} placement="top">
-                                <div>
-                                    <Rating name="read-only" value={company[0].averageSupervisionScore} readOnly />
-                                </div>
-                            </Tooltip>
+                            <Rating name="read-only" value={company[0].averageSupervisionScore} precision={0.5} readOnly />
                     </Typography>
 
                     <Typography className="oneCompanySubRatingLabels"> 
                             Learning 
-                            <Tooltip className="oneCompantSubRatingTooltop" title={company[0].averageLearningScore} placement="top">
-                                <div>
-                                    <Rating name="read-only" value={company[0].averageLearningScore} readOnly />
-                                </div>
-                            </Tooltip>
+                            <Rating name="read-only" value={company[0].averageLearningScore} precision={0.5} readOnly />
                     </Typography>
 
                     <Typography className="oneCompanySubRatingLabels"> 
                             Perks 
-                            <Tooltip className="oneCompantSubRatingTooltop" title={company[0].averagePerksScore} placement="top">
-                                <div>
-                                    <Rating name="read-only" value={company[0].averagePerksScore} readOnly />
-                                </div>
-                            </Tooltip>
+                            <Rating name="read-only" value={company[0].averagePerksScore} precision={0.5} readOnly />
                     </Typography>
 
                     <Typography className="oneCompanySubRatingLabels"> 
                             Culture 
-                            <Tooltip className="oneCompantSubRatingTooltop" title={company[0].averageCultureScore} placement="top">
-                                <div>
-                                    <Rating name="read-only" value={company[0].averageCultureScore} readOnly />
-                                </div>
-                            </Tooltip>
+                            <Rating name="read-only" value={company[0].averageCultureScore} precision={0.5} readOnly />
                     </Typography>
 
                     <Typography className="oneCompanySubRatingLabels"> 
                             Coding Practices 
-                            <Tooltip className="oneCompantSubRatingTooltop" title={company[0].averageCodingPracticesScore} placement="top">
-                                <div>
-                                    <Rating name="read-only" value={company[0].averageCodingPracticesScore} readOnly />
-                                </div>
-                            </Tooltip>
+                            <Rating name="read-only" value={company[0].averageCodingPracticesScore} precision={0.5} readOnly />
                     </Typography>
 
 
